d9da06964dde74eceb61a6c64a6fff9a7c00a61e
Chityanj <chityanj@gmail.com>
2020-10-18 18:59:50 +0530

    Create devices.js

---
+++ js/devices.js
@@ -0,0 +1,160 @@
+$(window).on('load', function() {
+		$.getJSON("https://raw.githubusercontent.com/Chityanj/downloads/master/maintaners.json?token=AE4MYLEUI4KY3Y5TN3QJENC7RNB6Y", function(data) {
+        data = data || [];
+        if (data.length == 0) {
+          return (document.getElementById("device_section").style.display =
+            "none");
+        }
+        var xia = 'Xiaomi';
+        var len = 'Lenovo';
+	    var one = 'OnePlus';
+	    var goo = 'Google';
+	    var real = 'Realme';
+	    var moto = 'Motorola';
+	    var leeco = 'LeEco';		
+	    var umi = 'UMIDIGI';
+	
+        for (var i = 0; i < data.length; i++) {
+        if (data[i].brand == xia){
+          $("#xiaomi").append(`
+			
+                      <a href="#/download/${data[i].codename}/">
+                            <section>
+				<img src="https://wiki.lineageos.org/images/devices/${data[i].codename}.png">
+                                <div class="blog_container">
+                                    <div class="section_title">${data[i].name}</div>
+                        <div class="about_section">${data[i].maintainer}</div>
+                    </div>
+                </section>
+            </a>
+            `);
+            }
+        }
+        for (var i = 0; i < data.length; i++) {
+            if (data[i].brand == len){
+              $("#lenovo").append(`
+                <a href="#/download/${data[i].codename}/">
+                    <section>
+			<img src="https://wiki.lineageos.org/images/devices/${data[i].codename}.png">
+                        <div class="blog_container">
+                            <div class="section_title">${data[i].name}</div>
+                            <div class="about_section">
+                                ${data[i].maintainer}
+                            </div>
+                        </div>
+                    </section>
+                </a>
+                
+                `);
+                }
+            }
+            for (var i = 0; i < data.length; i++) {
+                if (data[i].brand == goo){
+                  $("#google").append(`
+                    <a href="#/download/${data[i].codename}/">
+                        <section>
+			<img src="https://wiki.lineageos.org/images/devices/${data[i].codename}.png">
+                            <div class="blog_container">
+                                <div class="section_title">${data[i].name}</div>
+                                <div class="about_section">
+                                    ${data[i].maintainer}
+                                </div>
+                            </div>
+                        </section>
+                    </a>
+                    
+                    `);
+                    }
+                }
+			
+		for (var i = 0; i < data.length; i++) {
+                if (data[i].brand == leeco){
+                  $("#leeco").append(`
+                    <a href="#/download/${data[i].codename}/">
+                        <section>
+			<img src="https://wiki.lineageos.org/images/devices/${data[i].codename}.png">
+                            <div class="blog_container">
+                                <div class="section_title">${data[i].name}</div>
+                                <div class="about_section">
+                                    ${data[i].maintainer}
+                                </div>
+                            </div>
+                        </section>
+                    </a>
+                    
+                    `);
+                    }
+                }	
+                for (var i = 0; i < data.length; i++) {
+                        if (data[i].brand == one){
+                          $("#oneplus").append(`
+                            <a href="#/download/${data[i].codename}/">
+                            <section>
+				<img src="https://wiki.lineageos.org/images/devices/${data[i].codename}.png">
+                                <div class="blog_container">
+                                    <div class="section_title">${data[i].name}</div>
+                        <div class="about_section">${data[i].maintainer}</div>
+                    </div>
+                </section>
+            </a>
+                            `);
+                            }
+                        }
+                    for (var i = 0; i < data.length; i++) {
+                        if (data[i].brand == real){
+                          $("#realme").append(`
+                            <a href="#/download/${data[i].codename}/">
+                                <section>
+				<img src="https://wiki.lineageos.org/images/devices/${data[i].codename}.png">
+                                    <div class="blog_container">
+                                        <div class="section_title">${data[i].name}</div>
+                                        <div class="about_section">
+                                            ${data[i].maintainer}
+                                        </div>
+                                    </div>
+                                </section>
+                            </a>
+                            
+                            `);
+                            }
+                        }
+                        for (var i = 0; i < data.length; i++) {
+                            if (data[i].brand == moto){
+                              $("#motorola").append(`
+                                <a href="#/download/${data[i].codename}/">
+                                    <section>
+					<img src="https://wiki.lineageos.org/images/devices/${data[i].codename}.png">
+                                        <div class="blog_container">
+                                            <div class="section_title">${data[i].name}</div>
+                                            <div class="about_section">
+                                                ${data[i].maintainer}
+                                            </div>
+                                        </div>
+                                    </section>
+                                </a>
+                                
+                                `);
+                                }
+                            }
+                            for (var i = 0; i < data.length; i++) {
+                                if (data[i].brand == umi){
+                                  $("#umid").append(`
+                                    <a href="#/download/${data[i].codename}/">
+                                        <section>
+						<img src="https://wiki.lineageos.org/images/devices/${data[i].codename}.png">
+                                            <div class="blog_container">
+                                                <div class="section_title">${data[i].name}</div>
+                                                <div class="about_section">
+                                                    ${data[i].maintainer}
+                                                </div>
+                                            </div>
+                                        </section>
+                                    </a>
+                                    
+                                    `);
+                                    }
+                                }    
+      }).fail(function() {
+        return (document.getElementById("device_section").style.display = "none");
+      });
+});
