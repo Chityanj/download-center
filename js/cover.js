7f6b31030356726dc8c418a199e50a6ec234d41d
Chityanj <chityanj@gmail.com>
2020-10-18 21:42:19 +0530

    Update cover.js

--- js/cover.js
+++ js/cover.js
@@ -1,11 +1,11 @@
- function cover() {
+function cover() {
     var SL = ', 100%, 85%';
     var bgc =
       'linear-gradient(to left bottom, ' +
       "hsl(" + (Math.floor(Math.random() * 255) + SL) + ") 0%," +
       "hsl(" + (Math.floor(Math.random() * 255) + SL) + ") 100%)";
   
-  document.getElementsByClassName("section.cover.show").style.background = bgc;
+  document.getElementsByClassName('cover show')[0].style.background = bgc;
   
   
 }
