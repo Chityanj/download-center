143a4b595729e9221b20046b571c83660c02b9a0
Chityanj <chityanj@gmail.com>
2020-10-18 19:28:25 +0530

    Create cover.js

---
+++ js/cover.js
@@ -0,0 +1,18 @@
+/**
+   * Cover Page
+   * @returns {String} Cover page
+   */
+  function cover() {
+    var SL = ', 100%, 85%';
+    var bgc =
+      'linear-gradient(to left bottom, ' +
+      "hsl(" + (Math.floor(Math.random() * 255) + SL) + ") 0%," +
+      "hsl(" + (Math.floor(Math.random() * 255) + SL) + ") 100%)";
+
+    return (
+      "<section class=\"cover show\" style=\"background: " + bgc + "\">" +
+      '<div class="mask"></div>' +
+      '<div class="cover-main"><!--cover--></div>' +
+      '</section>'
+    );
+  }
