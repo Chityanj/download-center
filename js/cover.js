1f29d85d29be40d654e3b401b5cf0989a52b5777
Chityanj <chityanj@gmail.com>
2020-10-18 20:53:44 +0530

    Update cover.js

--- js/cover.js
+++ js/cover.js
@@ -1,15 +1,13 @@
-/**
-   * Cover Page
-   * @returns {String} Cover page
-   */
-  function cover() {
+ function cover() {
     var SL = ', 100%, 85%';
     var bgc =
       'linear-gradient(to left bottom, ' +
       "hsl(" + (Math.floor(Math.random() * 255) + SL) + ") 0%," +
       "hsl(" + (Math.floor(Math.random() * 255) + SL) + ") 100%)";
+  
+  document.getElementById("container").style.background = bgc;
+  
+  
+}
 
-    return (
-      'style="background:" + bgc + "'
-    );
-  }
+document.onload = cover();
