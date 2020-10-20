17412e32292c3ce4281650d946fd079a0547eae6
Chityanj <chityanj@gmail.com>
2020-10-20 22:59:22 +0530

    Update devices.js

--- js/devices.js
+++ js/devices.js
@@ -18,7 +18,7 @@ $(window).on('load', function() {
         if (data[i].brand == xia){
           $("#xiaomi").append(`
 			
-                      <a href="#/download/${data[i].codename}/">
+                      <a href="download/${data[i].codename}/">
                             <section>
 				<img src="https://wiki.lineageos.org/images/devices/${data[i].codename}.png" onerror="this.onerror=null;this.src='images/${data[i].codename}.webp';" />
                                 <div class="blog_container">
@@ -33,7 +33,7 @@ $(window).on('load', function() {
         for (var i = 0; i < data.length; i++) {
             if (data[i].brand == len){
               $("#lenovo").append(`
-                <a href="#/download/${data[i].codename}/">
+                <a href="download/${data[i].codename}/">
                     <section>
 			<img src="https://wiki.lineageos.org/images/devices/${data[i].codename}.png" onerror="this.onerror=null;this.src='images/${data[i].codename}.webp';" />
                         <div class="blog_container">
@@ -51,7 +51,7 @@ $(window).on('load', function() {
             for (var i = 0; i < data.length; i++) {
                 if (data[i].brand == goo){
                   $("#google").append(`
-                    <a href="#/download/${data[i].codename}/">
+                    <a href="download/${data[i].codename}/">
                         <section>
 			<img src="https://wiki.lineageos.org/images/devices/${data[i].codename}.png" onerror="this.onerror=null;this.src='images/${data[i].codename}.webp';" />
                             <div class="blog_container">
@@ -70,7 +70,7 @@ $(window).on('load', function() {
 		for (var i = 0; i < data.length; i++) {
                 if (data[i].brand == leeco){
                   $("#leeco").append(`
-                    <a href="#/download/${data[i].codename}/">
+                    <a href="download/${data[i].codename}/">
                         <section>
 			<img src="https://wiki.lineageos.org/images/devices/${data[i].codename}.png" onerror="this.onerror=null;this.src='images/${data[i].codename}.webp';" />
                             <div class="blog_container">
@@ -88,7 +88,7 @@ $(window).on('load', function() {
                 for (var i = 0; i < data.length; i++) {
                         if (data[i].brand == one){
                           $("#oneplus").append(`
-                            <a href="#/download/${data[i].codename}/">
+                            <a href="download/${data[i].codename}/">
                             <section>
 				<img src="https://wiki.lineageos.org/images/devices/${data[i].codename}.png" onerror="this.onerror=null;this.src='images/${data[i].codename}.webp';" />
                                 <div class="blog_container">
@@ -103,7 +103,7 @@ $(window).on('load', function() {
                     for (var i = 0; i < data.length; i++) {
                         if (data[i].brand == real){
                           $("#realme").append(`
-                            <a href="#/download/${data[i].codename}/">
+                            <a href="download/${data[i].codename}/">
                                 <section>
 				<img src="https://wiki.lineageos.org/images/devices/${data[i].codename}.png" onerror="this.onerror=null;this.src='images/${data[i].codename}.webp';" />
                                     <div class="blog_container">
@@ -121,7 +121,7 @@ $(window).on('load', function() {
                         for (var i = 0; i < data.length; i++) {
                             if (data[i].brand == moto){
                               $("#motorola").append(`
-                                <a href="#/download/${data[i].codename}/">
+                                <a href="download/${data[i].codename}/">
                                     <section>
 					<img src="https://wiki.lineageos.org/images/devices/${data[i].codename}.png" onerror="this.onerror=null;this.src='images/${data[i].codename}.webp';" />
                                         <div class="blog_container">
@@ -139,7 +139,7 @@ $(window).on('load', function() {
                             for (var i = 0; i < data.length; i++) {
                                 if (data[i].brand == umi){
                                   $("#umid").append(`
-                                    <a href="#/download/${data[i].codename}/">
+                                    <a href="download/${data[i].codename}/">
                                         <section>
 						<img src="https://wiki.lineageos.org/images/devices/${data[i].codename}.png" onerror="this.onerror=null;this.src='images/${data[i].codename}.webp';" />
                                             <div class="blog_container">
