ceeb94acc91be768029d753a15602971c30edf3e
Chityanj <chityanj@gmail.com>
2020-10-21 23:35:39 +0530

    Update downloadpg.js

--- js/downloadpg.js
+++ js/downloadpg.js
@@ -5,11 +5,11 @@ $(window).on('load', function() {
           return (document.getElementById("device_section").style.display =
             "none");
         }
-	    var title = document.getElementsByTagName("title")[0];
+	     var title = 'dipper';
         for (var i = 0; i < downpg.length; i++) {
         if (downpg[i].codename == title){
           $("#downimg").append(`
-				<img src="https://wiki.lineageos.org/images/devices/${downpg[i].codename}.png" onerror="this.onerror=null;this.src='../../images/${downpg[i].codename}.webp';" />
+	  <img src="https://wiki.lineageos.org/images/devices/${downpg[i].codename}.png" onerror="this.onerror=null;this.src='../../images/${downpg[i].codename}.webp';" />
                                 
             `);
             }
@@ -17,7 +17,7 @@ $(window).on('load', function() {
         for (var i = 0; i < downpg.length; i++) {
             if (downpg[i].codename == title){
               $("#devicehead").append(`
-                <h1>downpg[i].name</h1>
+                <h1>${downpg[i].name}</h1>
                 
                 `);
                 }
@@ -25,45 +25,11 @@ $(window).on('load', function() {
             for (var i = 0; i < downpg.length; i++) {
             if (downpg[i].codename == title){
               $("#buttondowpg").append(`
-                <a href="downpg[i].xda_thread" target="_blank" rel="noopener">GitHub</a>
+                <a href="${downpg[i].xda_thread}" target="_blank" rel="noopener">GitHub</a>
                 
                 `);
                 }
-            }
-      }).fail(function() {
-        return (document.getElementById("device_section").style.display = "none");
-      });
-});
- || [];
-        if (downpg.length == 0) {
-          return (document.getElementById("device_section").style.display =
-            "none");
-        }
-	    var title = document.getElementsByTagName("title")[0];
-        for (var i = 0; i < downpg.length; i++) {
-        if (downpg[i].codename == title){
-          $("#downimg").append(`
-				<img src="https://wiki.lineageos.org/images/devices/${downpg[i].codename}.png" onerror="this.onerror=null;this.src='../../images/${downpg[i].codename}.webp';" />
-                                
-            `);
-            }
-        }
-        for (var i = 0; i < downpg.length; i++) {
-            if (downpg[i].codename == title){
-              $("#devicehead").append(`
-                <h1>downpg[i].name</h1>
-                
-                `);
-                }
-            }
-            for (var i = 0; i < downpg.length; i++) {
-            if (downpg[i].codename == title){
-              $("#buttondowpg").append(`
-                <a href="downpg[i].xda_thread" target="_blank" rel="noopener">GitHub</a>
-                
-                `);
-                }
-            }
+            }      
       }).fail(function() {
         return (document.getElementById("device_section").style.display = "none");
       });
