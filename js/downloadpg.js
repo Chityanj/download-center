298d9edafd3c1d541c745bf91e8ea0353fb2c663
Chityanj <chityanj@gmail.com>
2020-10-22 00:30:55 +0530

    Update downloadpg.js

--- js/downloadpg.js
+++ js/downloadpg.js
@@ -12,24 +12,16 @@ $(window).on('load', function() {
 	  <img src="https://wiki.lineageos.org/images/devices/${downpg[i].codename}.png" onerror="this.onerror=null;this.src='../../images/${downpg[i].codename}.webp';" />
                                 
             `);
-            }
-        }
-        for (var i = 0; i < downpg.length; i++) {
-            if (downpg[i].codename == title){
-              $("#devicehead").append(`
+		$("#devicehead").append(`
                 <h1>${downpg[i].name}</h1>
                 
                 `);
-                }
-            }
-            for (var i = 0; i < downpg.length; i++) {
-            if (downpg[i].codename == title){
-              $("#buttondowpg").append(`
+		$("#buttondowpg").append(`
                 <a href="${downpg[i].xda_thread}" target="_blank" rel="noopener">GitHub</a>
                 
                 `);
-                }
-            }      
+            }
+        }
       }).fail(function() {
         return (document.getElementById("device_section").style.display = "none");
       });
