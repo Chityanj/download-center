b690026f27306dbd368c9d9cc1270814c15016d7
Chityanj <chityanj@gmail.com>
2020-10-23 23:18:54 +0530

    Update link.js

--- js/link.js
+++ js/link.js
@@ -4,16 +4,14 @@ var device = document.title;
              
        var day = dayjs.unix(emp.datetime)
 
-     var formatted = moment(day).format('L');
-
-      console.log(formatted); // "02/24/2018"
+    var formatted = dayjs(day).format('DD/MM/YYYY')
        
        var inMB = (emp.size/1048576).toFixed(1) + 'MB' 
             $("#intro").append(`
                 <h5>Version: ${emp.version}</h5>                       
                `);
       
-              $("#intro").append( "<h5>Build: " + formatted + "</h5>");
+              $("#intro").append( "<h5>Date: " + formatted + "</h5>");
               $("#intro").append( "<h5>Size: " + inMB + "</h5>");
        
               $("#buttons").append(`<a href="${emp.url}"><button type="button" class="btn btn blue-gradient">Download</button></a>`);
