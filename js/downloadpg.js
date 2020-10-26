8898a96fb55b90c15b57a200bdf415fb21d7e5d2
Chityanj <chityanj@gmail.com>
2020-10-26 19:54:12 +0530

    Update downloadpg.js

--- js/downloadpg.js
+++ js/downloadpg.js
@@ -1,5 +1,6 @@
 $(window).on('load', function() {
-		$.getJSON("https://raw.githubusercontent.com/Chityanj/downloads/master/maintaners.json?token=AE4MYLAQAWPPMRBGSBFUXYS7SV7HQ", function(downpg) {
+	        var device = document.title;
+		$.getJSON("https://raw.githubusercontent.com/Chityanj/downloads/master/maintaners.json?token=AE4MYLB47XWE23QIHZKFDJC7UALG4", function(downpg) {
         downpg = downpg || [];
         if (downpg.length == 0) {
           return (document.getElementById("device_section").style.display =
@@ -7,17 +8,17 @@ $(window).on('load', function() {
         }
 	    var title = document.title;
         for (var i = 0; i < downpg.length; i++) {
-        if (downpg[i].codename == title){
+        if (downpg[i].codename == device){
           $("#downimg").append(`
 	  <img src="https://wiki.lineageos.org/images/devices/${downpg[i].codename}.png" onerror="this.onerror=null;this.src='../../images/${downpg[i].codename}.webp';" />
                                 
             `);
-		$("#devicehead").append(`
-                <h1>${downpg[i].name}</h1>
+		$("#title").append(`
+                ${downpg[i].name}
                 
                 `);
-		$("#buttondowpg").append(`
-                <a href="${downpg[i].xda_thread}" target="_blank" rel="noopener">XDA</a>
+		$("#buttons").append(`
+                <a href="${downpg[i].xda_thread}"><button type="button" class="btn peach-gradient">XDA</button></a>
                 
                 `);
             }
@@ -25,4 +26,23 @@ $(window).on('load', function() {
       }).fail(function() {
         return (document.getElementById("device_section").style.display = "none");
       });
+
+
+     $.getJSON( "https://raw.githubusercontent.com/RevengeOS-Devices/official_devices/r10.0/" + device + "/device.json", function(emp) {
+             
+       var day = dayjs.unix(emp.datetime)
+
+    var formatted = dayjs(day).format('DD/MM/YYYY')
+       
+       var inMB = (emp.size/1048576).toFixed(1) + 'MB' 
+            $("#ver-value").append(`
+                <h6>${emp.version}</h6><br>                      
+               `);
+      
+              $("#date-value").append( "<h6>" + formatted + "</h6>");
+              $("#size-value").append( "<h6>Size: " + inMB + "</h6>");
+       
+              $("#buttons").append(`<a href="${emp.url}"><button type="button" class="btn blue-gradient">Download</button></a>`);
+       
+       }); 
 });
