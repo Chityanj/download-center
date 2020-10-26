426717cc0076bd1b4d6e4813898b690f676488a8
Chityanj <chityanj@gmail.com>
2020-10-26 19:57:37 +0530

    Update index.php

--- download/x2/index.php
+++ download/x2/index.php
@@ -24,7 +24,6 @@ function getTitle($t){
 	<script src="https://unpkg.com/dayjs@1.8.21/dayjs.min.js"></script>
 	<script>dayjs().format()</script>
       	<script src="../../js/downloadpg.js"></script>
-	   <script src="../../js/link.js"></script>
 
 	<!-- Bootstrap tooltips -->
 	<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.4/umd/popper.min.js"></script>
@@ -49,28 +48,28 @@ function getTitle($t){
             <div class="main-card">
                 <header id="page-header" class="text-center">
                     <a href="/" class="btn blue-gradient nav-btn fas fa-angle-left left-0" role="button"></a>
-                    <h2 id="title" class="metropolis-medium">LeEco Le Max 2</h2>
+                    <h2 id="title" class="metropolis-medium"></h2>
                 </header>
                 <div class="container">
                     <div class="row device-section">
-                        <div class="col-sm-5">
-                            <img src="https://wiki.lineageos.org/images/devices/x2.png" id="device-image" class="device-image spec-device-img">
+                        <div id="downimg" class="col-md">
+                            
                         </div>
-                        <div class="col-sm-5">
+                        <div class="col-md">
                             
                             <div class="row">
                                 <div class="col spec-col">
                                     <i class="icon far fa-calendar-alt"></i>
                                     <div class="spec-text">
                                         <div class="metropolis-semibold spec-type">Date</div>
-                                        <div id="cpu-value" class="spec-value">-</div>
+                                        <div id="date-value" class="spec-value"></div>
                                     </div>
                                 </div>
                                 <div class="col spec-col">
                                     <i class="icon fas fa-info-circle"></i>
                                     <div class="spec-text">
                                         <div class="metropolis-semibold spec-type">Version</div>
-                                        <div id="ram-value" class="spec-value">-</div>
+                                        <div id="ver-value" class="spec-value">-</div>
                                     </div>
                                 </div>
                             </div>
@@ -79,7 +78,7 @@ function getTitle($t){
                                     <i class="icon fas fa-download"></i>
                                     <div class="spec-text">
                                         <div class="metropolis-semibold spec-type">Size</div>
-                                        <div id="display-value" class="spec-value">-</div>
+                                        <div id="size-value" class="spec-value">-</div>
                                     </div>
                                 </div>
                                 <div class="col spec-col">
@@ -95,47 +94,15 @@ function getTitle($t){
                     </div>
                    
             </div>
-              <div class="text-center">
+              <div id="buttons" class="text-center">
 
-  <button type="button" class="btn blue-gradient" data-toggle="modal" data-target="#modalYT">Download</button>
-  <button type="button" class="btn peach-gradient" data-toggle="modal" data-target="#modalVM">XDA</button>
+  
   <button type="button" class="btn purple-gradient" data-toggle="modal" data-target="#modalGM">Changelog</button>
 
 </div>
         </div>
 
-        <script src="https://unpkg.com/jquery@3/dist/jquery.min.js" crossorigin="anonymous"></script>
-        <script type="text/javascript" src="/js/brands.js<!--#include virtual=" common="" version.txt"="" --="">"></script>
-        <script type="text/javascript" src="/js/utils.js<!--#include virtual=" common="" version.txt"="" --="">"></script>
-        <script type="text/javascript" src="/js/device.js<!--#include virtual=" common="" version.txt"="" --="">"></script>
-        <script type="text/javascript" src="/js/ui.js<!--#include virtual=" common="" version.txt"="" --="">"></script>
-        <!-- Global site tag (gtag.js) - Google Analytics -->
-        <script async="" src="https://www.googletagmanager.com/gtag/js?id=UA-136221811-1"></script>
-        <script>
-            window.dataLayer = window.dataLayer || [];
-
-            function gtag() {
-                dataLayer.push(arguments);
-            }
-            gtag('js', new Date());
-
-            gtag('config', 'UA-136221811-1');
-        </script>
-        <script async="" src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
-        <script>
-            (adsbygoogle = window.adsbygoogle || []).push({
-                google_ad_client: "ca-pub-5878875805175242",
-                enable_page_level_ads: true
-            });
-        </script>
-    <script type="text/javascript" language="javascript">
-      var aax_size='728x90';
-      var aax_pubname = 'revengeos-21';
-      var aax_src='302';
-    </script>
-    <script type="text/javascript" language="javascript" src="http://c.amazon-adsystem.com/aax2/assoc.js"></script>
-
-
+        
 
 
 </div>
