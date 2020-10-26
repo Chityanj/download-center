e0f7f78cf8f15d880b9105e4f314e96e95c78bae
Chityanj <chityanj@gmail.com>
2020-10-26 13:22:03 +0530

    Update index.php

--- download/x2/index.php
+++ download/x2/index.php
@@ -38,7 +38,7 @@ function getTitle($t){
 	<link href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.5.0/css/bootstrap.min.css" rel="stylesheet">
 	<!-- Material Design Bootstrap -->
 	<link href="https://cdnjs.cloudflare.com/ajax/libs/mdbootstrap/4.19.1/css/mdb.min.css" rel="stylesheet">   
-      <link rel="stylesheet" href="../../css/styles.css" title="vue">
+      <link rel="stylesheet" href="../../css/devices.css" title="vue">
       
       <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
       
