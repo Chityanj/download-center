3d55311fc789934937f62bdd44567c11658da449
chityanj <chityanjs@gmail.com>
2020-10-27 06:30:50 +0000

    get ready

--- download/x2/title.php
+++
@@ -1,8 +0,0 @@
-<?php
-function getTitle($t){
-    $title = basename($t);
-    $title = str_replace('_', ' ', $title);//replacing '_' by spaces
-    return $title;
-}
-?>
-<title><?php echo getTitle(__DIR__); ?></title>
