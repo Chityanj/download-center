6d3b650c1f5691b1fa8028cbd175d86ab53769fd
chityanj <chityanj@gmail.com>
2020-11-10 19:28:05 +0530

    add files

---
+++ copy.sh
@@ -0,0 +1,5 @@
+git clone https://github.com/RevengeOS-Devices/official_devices.git download
+for f in $PWD/download/*;
+  do 
+     [ -d $f ] && cd "$f" && echo Entering into $f && cp ~/download-center/assets/downloadpg/index.php  "$f"
+  done;
