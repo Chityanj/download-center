00987a5e45c7bf3a09f117bb102319364d8c5ce1
chityanj <chityanj@gmail.com>
2020-11-14 21:23:39 +0530

    add kunlun

--- copy.sh
+++ copy.sh
@@ -1,5 +1,7 @@
+rm -rf download
 git clone https://github.com/RevengeOS-Devices/official_devices.git download
 for f in $PWD/download/*;
   do 
      [ -d $f ] && cd "$f" && echo Entering into $f && cp ~/download-center/assets/downloadpg/index.php  "$f"
   done;
+
